d1f2fa79ba966067ebe0123e57dd597333fd569b
sounsaran <sounsaran4@gmail.com>
2023-11-23 22:08:14 +0700

    test

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-< lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,45 +10,53 @@
     <nav class="navbar navbar-expand-sm bg-nav">        
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Logo</a>
-            <ul class="navbar-nav" >
-                <li class="nav-item">
-                    <a class="nav-link text-info" href="#">Link 1</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-info" href="#">Link 2</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-info" href="#">Link 3</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-info" role="button" data-bs-toggle="dropdown">Dropdown</a>
-                    <ul class="dropdown-menu" id="dropdown">
-                        <li><a class="dropdown-item text-info" href="#">Link</a></li>
-                        <li><a class="dropdown-item text-info" href="#">Another link</a></li>
-                        <li><a class="dropdown-item text-info" href="#">A third link</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-info" href="#">Link 5</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-info" href="#">Link 6</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-info" href="#">Link 7</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-info" href="#">Link 8</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-info" href="#">Link 9</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-info" href="#">Link 10</a>
-                </li>
-            </ul>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <i class="fa-solid fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav" >
+                    <li class="nav-item">
+                        <a class="nav-link text-info" href="#home">Link 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-info" href="#">Link 2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-info" href="#">Link 3</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-info" role="button" data-bs-toggle="dropdown">Dropdown</a>
+                        <ul class="dropdown-menu" id="dropdown">
+                            <li><a class="dropdown-item text-info" href="#">Link</a></li>
+                            <li><a class="dropdown-item text-info" href="#">Another link</a></li>
+                            <li><a class="dropdown-item text-info" href="#">A third link</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-info" href="#">Link 5</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-info" href="#">Link 6</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-info" href="#">Link 7</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-info" href="#">Link 8</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-info" href="#">Link 9</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-info" href="#">Link 10</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
+    <section id="home">
+        sdfghcf
+    </section>
 </body>
 </html>
 
