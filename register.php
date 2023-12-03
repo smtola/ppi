ad16518ec347ebe3117910c8bf903a33af965e0c
somtola.smt@gmail.com <somtola>
2023-12-02 18:00:04 -0800

    new update

--- register.php
+++ register.php
@@ -11,12 +11,12 @@
 </head>
 
 <body class="d-flex justify-content-center align-items-center" style="min-height:100vh;">
-    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal"> Register</button>
+    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mbModal"> Register</button>
 
 
     <!-- The Modal -->
-    <div class="modal" id="myModal">
-        <div class="modal-dialog modal-dialog-centered">
+    <div class="modal" id="mbModal">
+        <div class="modal-dialog modal-dialog-centered modal-fullscreen-md">
             <div class="modal-content">
 
                 <!-- Modal Header -->
@@ -26,61 +26,70 @@
 
                 <!-- Modal body -->
                 <div class="modal-body ">
-                    <form>
-                    <div class="row">
-                        <div class="col-lg-6 my-1">
-                                <label for="name" class="col-lg-12 control-label">លេខអត្តសញ្ញាណ</label>
-                                <input type="number" class="form-control" name="name" id="name">
-                                <label for="name" class="col-lg-12 control-label">ភេទ</label>
-                                <select class="form-select">
+                    <form id="validators">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <label for="id" class="col-lg-12 control-label">លេខអត្តសញ្ញាណ</label>
+                                <input type="number" class="form-control form-control-sm mb-1" name="id" id="id" required>
+
+                                <label for="gender" class="col-lg-12 control-label">ភេទ</label>
+                                <select class="form-select form-select-sm mb-1" name="gender" id="gender" required>
                                     <option value="male">ប្រុស</option>
                                     <option value="female">ស្រី</option>
                                     <option value="other">ផ្សេងៗ</option>
                                 </select>
                             </div>
-                            <div class="col-lg-6 my-1">
-                                <label for="name" class="col-lg-12 control-label">ឈ្មោះ</label>
-                                <input class="form-control" name="name" id="name">
-                                <label type="text" for="name" class="col-lg-12 control-label">អាយុ</label>
-                                <input type="number" class="form-control" name="name" id="name">
-                            </div>
-                    </div>
 
-                    <div class="row">
-                        <div class="col-lg-6 my-1">
-                                <label for="name" class="col-lg-12 control-label">អីម៉ែល</label>
-                                <input type="email" class="form-control" name="name" id="name">
-                                <label for="name" class="col-lg-12 control-label">ថ្ងៃខែឆ្នាំកំណើត</label>
-                                <input type="date" class="form-control" name="name" id="name">
+                            <div class="col-lg-6">
+                                <label for="name" class="col-lg-12 control-label">ឈ្មោះ</label>
+                                <input class="form-control form-control-sm mb-1" name="name" id="name" required>
+
+                                <label type="text" for="name" class="col-lg-12 control-label">អាយុ</label>
+                                <input type="number" class="form-control form-control-sm mb-1" name="name" id="name" required>
+                            </div>
                         </div>
-                        <div class="col-lg-6 my-1">
-                            <label for="name" class="col-lg-12 control-label">លេខទូរស័ព្ទ</label>
-                            <input type="number" class="form-control" name="name" id="name">
-                            <label for="name" class="col-lg-12 control-label">ជំនាញ</label>
-                            <select class="form-select">
-                                <option value="male">a</option>
+
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <label for="name" class="col-lg-12 control-label">អីម៉ែល</label>
+                                <input type="email" class="form-control form-control-sm mb-1" name="name" id="name" required>
+
+                                <label for="name" class="col-lg-12 control-label">ថ្ងៃខែឆ្នាំកំណើត</label>
+                                <input type="date" class="form-control form-control-sm mb-1" name="name" id="name" required>
+                            </div>
+
+                            <div class="col-lg-6">
+                                <label for="name" class="col-lg-12 control-label">លេខទូរស័ព្ទ</label>
+                                <input type="number" class="form-control form-control-sm mb-1" name="name" id="name" required>
+
+                                <label for="name" class="col-lg-12 control-label ">ជំនាញ</label>
+                                <select class="form-select  form-select-sm mb-1" required>
+                                    <option value="male">a</option>
                                     <option value="female">b</option>
                                     <option value="other">c</option>
                                 </select>
                             </div>
                         </div>
-                    
+
                         <div class="row">
-                        <div class="col-lg-6 my-1">
-                            <label for="name" class="col-lg-12 control-label">ទីកន្លែងកំណើត</label>
-                            <textarea class="form-control" rows="1" id="comment" name="text"></textarea>
-                            <label for="name" class="col-lg-12 control-label">អាស័យដ្ឋានបច្ចុប្បន្ន</label>
-                            <textarea class="form-control" rows="1" id="comment" name="text"></textarea>
-                        </div>
-                        <div class="col-lg-6 my-1">
-                            <label for="name" class="col-lg-12 control-label">ថ្នាក់</label>
-                            <select class="form-select">
-                                <option value="male">a</option>
-                                <option value="female">b</option>
-                                <option value="other">c</option>
-                            </select>
-                            <label for="name" class="col-lg-12 control-label">វេន</label>
-                            <select class="form-select">
+                            <div class="col-lg-6">
+                                <label for="name" class="col-lg-12 control-label">ទីកន្លែងកំណើត</label>
+                                <textarea class="form-control form-control-sm mb-1" rows="1" id="comment" name="text" required></textarea>
+
+                                <label for="name" class="col-lg-12 control-label">អាស័យដ្ឋានបច្ចុប្បន្ន</label>
+                                <textarea class="form-control form-control-sm mb-1" rows="1" id="comment" name="text" required></textarea>
+                            </div>
+
+                            <div class="col-lg-6">
+                                <label for="name" class="col-lg-12 control-label">ថ្នាក់</label>
+                                <select class="form-select  form-select-sm mb-1" required>
+                                    <option value="male">a</option>
+                                    <option value="female">b</option>
+                                    <option value="other">c</option>
+                                </select>
+                                <label for="name" class="col-lg-12 control-label">វេន</label>
+
+                                <select class="form-select  form-select-sm mb-1" required>
                                     <option value="male">a</option>
                                     <option value="female">b</option>
                                     <option value="other">c</option>
@@ -88,27 +97,28 @@
                             </div>
                         </div>
                     </form>
-                    </div>
+                </div>
 
                 <!-- Modal footer -->
                 <div class="modal-footer">
-                        <button type="button" class="btn btn-outline-primary btn-sm" data-bs-dismiss="modal" style="display: flex; gap: 5px;">
-                            <i class='bx bx-save' style='font-size: 20px;'></i>
-                            Save
-                        </button>
+                    <button type="submit" class="btn btn-outline-primary btn-sm" style="display: flex; gap: 5px;">
+                        <i class='bx bx-save' style='font-size: 20px;'></i>
+                        Save
+                    </button>
 
-                        <button type="button" class="btn btn-outline-danger btn-sm" data-bs-dismiss="modal" style="display: flex; gap: 5px;">
-                            <i class='bx bx-x' style='font-size: 20px;'></i>
-                            Close
-                        </button>
+                    <button type="button" class="btn btn-outline-danger btn-sm" data-bs-dismiss="modal" style="display: flex; gap: 5px;">
+                        <i class='bx bx-x' style='font-size: 20px;'></i>
+                        Close
+                    </button>
                 </div>
 
             </div>
         </div>
     </div>
 </body>
-
 </html>
 <script src="assets/bootstrap/js/jquery3.6.js"></script>
 <script src="assets/bootstrap/js/popper.min.js"></script>
 <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/validation.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
