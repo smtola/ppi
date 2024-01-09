36f1d4dbea9eaa74a7093afbe66844cb2bc0b8a0
somtola <somtola.smt@gmail.com>
2024-01-09 16:02:09 +0700

    new update

---
+++ assets/js/scripts.js
@@ -0,0 +1,26 @@
+
+function onDropdown(){
+  document.getElementById('myDropdown').classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown-btn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+// navbar theme
+
+window.onscroll = function () { 
+    if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200  ) {
+      navbar.classList.add("changeThemeNav");
+    } 
+    else {
+      navbar.classList.remove("changeThemeNav");
+    }
+};
